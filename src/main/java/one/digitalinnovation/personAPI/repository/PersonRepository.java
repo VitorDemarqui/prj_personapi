ac0c05ccc08093323e9754bd1de4d991dd573dff
vitor <vitordemarqui@gmail.com>
2021-07-21 22:25:54 -0300

    'Api get, put, delete and mapping developed'

--- src/main/java/one/digitalinnovation/personAPI/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package one.digitalinnovation.personAPI.repository;
-
-import one.digitalinnovation.personAPI.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//extende uma interface jpa que e proprio do banco de dados
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
